6f40a08cc1ae8a5b2830630eb966f485eaac95cf
Thiago Peixoto <thiago.peixots@gmail.com>
2025-03-19 20:26:52 -0300

    Adjust in model user

--- src/main/java/com/tpodontologia/backend/model/User.java
+++ src/main/java/com/tpodontologia/backend/model/User.java
@@ -6,17 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
+import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-
-@AllArgsConstructor
-@ToString
+@NoArgsConstructor
 @Entity
+@Data
 
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "Preencha seu nome inteiro!")
     private String name;
@@ -29,61 +30,10 @@ public class User {
     @NotNull(message = "Preencha sua idade!")
     private int age;
     @NotBlank(message = "Preencha seu CPF.")
-    @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
-    private int cpf;
-    @NotBlank(message = "Preencha seu número para contato.")
-    private int phone;
+    @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "CPF inválido")
+    private String cpf;
 
+    @Pattern(regexp = "\\d{10,11}", message = "Número de telefone inválido")
+    private String phone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
 }
